e1097ca36cd0669f3e018e87efe01d464b74d4a9
Savant Krishna <savant.2020@gmail.com>
2015-02-17 11:12:29 +0530

    Modify to deal with C++11 warning : Narrowing conversion of int to char

--- jquery/charval.py
+++ jquery/charval.py
@@ -1,6 +1,6 @@
 import sys
 jq = open('jquery.min.js')
 print 'const char JQUERY_STR[] = {'
-character_vals = [ "%d"%ord(c) for x in jq  for c in x ]
+character_vals = [ "static_cast<char>(%d)"%ord(c) for x in jq  for c in x ]
 print ','.join(character_vals)
 print '};'
